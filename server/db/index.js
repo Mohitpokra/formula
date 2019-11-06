c77890d4095632c20fe5d11c957a431dec214721
Mohit Gupta <mohit@shakedeal.com>
2019-11-06 19:08:18 +0530

    master: add dburl and update package file

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     try {
-       await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true,  useCreateIndex: true });
+       await mongoose.connect('mongodb+srv://mgpokra:Mongo@27607@cluster0-dqzy6.mongodb.net/formula?retryWrites=true&w=majority', { useNewUrlParser: true,  useCreateIndex: true });
        console.log('mongodb Connected......');
     } catch(err) {
         console.log(err.message);
