0a16679940c8c9d8ea2b0210fc2703e78b6d3fd3
Mohit Gupta <mohit@shakedeal.com>
2019-12-08 12:59:21 +0530

    master: navagation buttonn shifted to navbar and repalced with app drawer button

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,50 +9,84 @@ module.exports = {
   },
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      },
+      {
+        name: "theme-color",
+        content: "#2196F3"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
-      { rel:"stylesheet", href:"https://cdn.jsdelivr.net/npm/animate.css@3.5.1" }
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      },
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/animate.css@3.5.1"
+      }
     ],
-    script: [
-      {src: "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_CHTML"},
-      {type: "text/x-mathjax-config", src:"@/asset/mathjax.config"}
+    script: [{
+        src: "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_CHTML"
+      },
+      {
+        type: "text/x-mathjax-config",
+        src: "@/asset/mathjax.config"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#FFFFFF' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#FFFFFF'
+  },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    {src: '@/assets/css/custom.scss'}
+   ** Global CSS
+   */
+  css: [{
+    src: '@/assets/css/custom.scss'
+  }],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{
+      src: '@/plugins/filter.js'
+    },
+    {
+      src: '@/plugins/carousel.js',
+      mode: 'client'
+    },
+    {
+      src: '@/plugins/mathjax/index.client.js',
+      ssr: false
+    },
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    { src:  '@/plugins/filter.js'},
-    { src:  '@/plugins/carousel.js',  mode: 'client'},
-    { src: '@/plugins/mathjax/index.client.js', ssr: false},
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
@@ -74,19 +108,19 @@ module.exports = {
     }
   },
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
